797ce90c5b95b7ea364018ea6f5165669fa19a67
Tatsuki Ishigaki <e165733@ie.u-ryukyu.ac.jp>
2017-01-10 14:47:25 +0900

    step1

---
+++ src/main/java/jp/ac/uryukyu/ie/e165733/Hero.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e165733;
+
+public class Hero extends LivingThing{
+    public Hero (String name, int hitPoint, int attack) {
+        super(name,hitPoint,attack);
+    }
+
+    public void wounded(int damage){
+        setHitPoint(getHitPoint() - damage);
+        if( getHitPoint() < 0 ) {
+            setDead(true);
+            System.out.printf("%sは道半ばで力尽きた\n", getName());
+        }
+    }
+}
